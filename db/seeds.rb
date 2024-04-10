060a0a2b18a3ff11c21b9ba7e0c52c3167efb9bf
mark19242 <mark19242@gmail.com>
2024-04-10 16:49:32 -0700

    seeds planted

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,38 @@
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+user1 = User.where(email: "test1@example.com").first_or_create(password: "password", password_confirmation: "password")
+user2 = User.where(email: "test2@example.com").first_or_create(password: "password", password_confirmation: "password")
+
+apartments = [
+  {
+    street: '129 West 81st Street',
+    unit: '5A',
+    city: 'NY',
+    state: 'NY',
+    square_footage: '1000',
+    price: '2000',
+    bedrooms: 1,
+    bathrooms: 1.0,
+    pets: 'no pets allowed',
+    image: 'https://www.hollywoodreporter.com/wp-content/uploads/2015/06/seinfeld_apt.jpg'
+  },
+  {
+    street: '21 Jumpstreet',
+    unit: '6C',
+    city: 'SA',
+    state: 'TX',
+    square_footage: '2500',
+    price: '1500',
+    bedrooms: 2,
+    bathrooms: 1.5,
+    pets: 'pets allowed',
+    image: 'https://www.hollywoodreporter.com/wp-content/uploads/2015/06/seinfeld_apt.jpg'
+  },
+]
+
+apartments.each do |apartment|
+  user1.apartments.create(apartment)
+  puts "Creating: #{apartment}"
+end
+
