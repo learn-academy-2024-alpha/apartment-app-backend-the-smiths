6d7fbd3080f4112e76d2f2f892ae08cf930212e3
smorgannicole <morgann.smithh@yahoo.com>
2024-04-17 11:56:30 -0700

    spaces were added in seeds and tests refactored

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,6 +1,8 @@
 class Apartment < ApplicationRecord
   belongs_to :user
-  validates :street, :unit, :city, :state, :square_footage, :price, :bedrooms, :bathrooms, :pets, :smart_security_system, :rooftop_pool, :golf_course, :community_garden, :keyless_entry, :onsite_daycare, :smart_thermostat, :spa, :concierge, :ocean_view, :personal_chef, :fiber_optic, :maid_service, :pizza_ovens, :hibachi_grill, :full_kitchen, presence: true
+  validates :name, :street, :unit, :city, :state, :square_footage, :price, :bedrooms, :bathrooms, :pets, :image, presence: true
+  validates :smart_security_system, :rooftop_pool, :golf_course, :community_garden, :keyless_entry, :onsite_daycare, :smart_thermostat, :spa, :concierge, :ocean_view, :personal_chef, :fiber_optic, :maid_service, :pizza_ovens, :hibachi_grill, :full_kitchen, inclusion: [true, false]
+  validates :smart_security_system, :rooftop_pool, :golf_course, :community_garden, :keyless_entry, :onsite_daycare, :smart_thermostat, :spa, :concierge, :ocean_view, :personal_chef, :fiber_optic, :maid_service, :pizza_ovens, :hibachi_grill, :full_kitchen, exclusion: [nil]
 end
 
 
